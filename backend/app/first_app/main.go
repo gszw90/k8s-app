954fe02916f65fde06acd650ae19eb89a023aac5
zeng <13350016962@163.com>
2025-10-15 17:41:34 +0800

    Adds system info endpoint to API server
    Introduces a new route providing runtime environment details,
    including OS, architecture, goroutine count, Go version, CPU count,
    and current server time to aid diagnostics and monitoring.

--- backend/app/first_app/main.go
+++ backend/app/first_app/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,9 +14,8 @@ func main() {
 	startServer()
 }
 
-
-func startServer(){
-	r:= gin.Default()
+func startServer() {
+	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -23,6 +24,19 @@ func startServer(){
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world,this is first app",
+		})
+	})
+	r.GET("/info", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "info",
+			"data": map[string]interface{}{
+				"os":         runtime.GOOS,
+				"arch":       runtime.GOARCH,
+				"goroutines": fmt.Sprintf("%d", runtime.NumGoroutine()),
+				"version":    runtime.Version(),
+				"cpus":       runtime.NumCPU(),
+				"time":       time.Now().Format("2006-01-02 15:04:05"),
+			},
 		})
 	})
 	// Listen and serve on
