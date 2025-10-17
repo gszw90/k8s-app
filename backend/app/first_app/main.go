7be56170a8557c27418f633f47e2108c987898af
zeng <13350016962@163.com>
2025-10-17 17:42:32 +0800

    test cicd

--- backend/app/first_app/main.go
+++ backend/app/first_app/main.go
@@ -52,7 +52,7 @@ func startServer() {
 	})
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "hello world,this is first app",
+			"message": "hello world,this is first app 233",
 		})
 	})
 	r.GET("/info", func(ctx *gin.Context) {
